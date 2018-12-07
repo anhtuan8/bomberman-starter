c6c41d98b4a1985cc28b06848c9eb366f6c0d4e7
OS <anhtuan.051998@gmail.com>
2018-12-07 13:33:35 +0700

    soundfx

--- src/uet/oop/bomberman/entities/character/enemy/Oneal.java
+++ src/uet/oop/bomberman/entities/character/enemy/Oneal.java
@@ -4,6 +4,7 @@ package uet.oop.bomberman.entities.character.enemy;
 import uet.oop.bomberman.Board;
 import uet.oop.bomberman.Game;
 import uet.oop.bomberman.entities.character.enemy.ai.AIMedium;
+import uet.oop.bomberman.entities.character.enemy.ai.AvoidBombAI;
 import uet.oop.bomberman.graphics.Sprite;
 
 import java.util.ArrayList;
@@ -50,83 +51,86 @@ public class Oneal extends Enemy {
 	public void calculateMove() {
 		double xa=0,ya=0;
 		Random random = new Random();
-		if(stand < -5) {
+		if (stand < -5) {
 			condition = -2;
 			stand = 0;
 		}
-		if(_steps <=0){
-			ArrayList<Integer> choices = new ArrayList<>(Arrays.asList(0,1,2,3));
-			switch (condition){
-				case -1:{
-					_direction = _ai.calculateDirection();
-					break;
-				}
-				
-				case -2:{
-					_direction = random.nextInt(4);
-					condition = _direction;
-					break;
-				}
-				case 0:{
-					choices.remove(Integer.valueOf(2));
-					if(canMove(1,0) | canMove(-1,0)) {
-						choices.remove(Integer.valueOf(0));
-						_direction = choices.get(random.nextInt(choices.size()));
-						condition = -1;
+		if (_steps <= 0) {
+			if(_board.getBombs().size() > 0){
+				AvoidBombAI ai = new AvoidBombAI(_board.getBombs().get(0),this);
+				_direction = ai.calculateDirection();
+			}
+			else {
+				ArrayList<Integer> choices = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
+				switch (condition) {
+					case -1: {
+						_direction = _ai.calculateDirection();
+						break;
 					}
-					else _direction = condition;
-					break;
-				}
-				case 2:{
-					choices.remove(Integer.valueOf(0));
-					if(canMove(1,0) | canMove(-1,0)) {
+
+					case -2: {
+						_direction = random.nextInt(4);
+						condition = _direction;
+						break;
+					}
+					case 0: {
 						choices.remove(Integer.valueOf(2));
-						_direction = choices.get(random.nextInt(choices.size()));
-						condition = -1;
+						if (canMove(1, 0) | canMove(-1, 0)) {
+							choices.remove(Integer.valueOf(0));
+							_direction = choices.get(random.nextInt(choices.size()));
+							condition = -1;
+						} else _direction = condition;
+						break;
 					}
-					else _direction = condition;
-					break;
-				}
-
-				case 1:{
-					choices.remove(Integer.valueOf(3));
-					if(canMove(0,1) | canMove(0,-1)) {
-						choices.remove(Integer.valueOf(1));
-						_direction = choices.get(random.nextInt(choices.size()));
-						condition = -1;
+					case 2: {
+						choices.remove(Integer.valueOf(0));
+						if (canMove(1, 0) | canMove(-1, 0)) {
+							choices.remove(Integer.valueOf(2));
+							_direction = choices.get(random.nextInt(choices.size()));
+							condition = -1;
+						} else _direction = condition;
+						break;
 					}
-					else _direction = condition;
-					break;
 
-				}
-				case 3:{
-					choices.remove(Integer.valueOf(1));
-					if(canMove(0,1) | canMove(0,-1)) {
+					case 1: {
 						choices.remove(Integer.valueOf(3));
-						_direction = choices.get(random.nextInt(choices.size()));
-						condition = -1;
-					}
-					else _direction = condition;
-					break;
+						if (canMove(0, 1) | canMove(0, -1)) {
+							choices.remove(Integer.valueOf(1));
+							_direction = choices.get(random.nextInt(choices.size()));
+							condition = -1;
+						} else _direction = condition;
+						break;
 
+					}
+					case 3: {
+						choices.remove(Integer.valueOf(1));
+						if (canMove(0, 1) | canMove(0, -1)) {
+							choices.remove(Integer.valueOf(3));
+							_direction = choices.get(random.nextInt(choices.size()));
+							condition = -1;
+						} else _direction = condition;
+						break;
+					}
 				}
 			}
 			_steps = MAX_STEPS;
 		}
-		if(_direction == 0) ya = - 1;
-		if(_direction == 1) xa = + 1;
-		if(_direction == 2) ya = + 1;
-		if(_direction == 3) xa = - 1;
 
-		if(canMove(xa,ya)){
-			_steps = _steps - 1 -rest;
-			move(xa*_speed,ya*_speed);
+		if (_direction == 0) ya = -1;
+		if (_direction == 1) xa = +1;
+		if (_direction == 2) ya = +1;
+		if (_direction == 3) xa = -1;
+
+		if (canMove(xa, ya)) {
+			_steps = _steps - 1 - rest;
+			move(xa * _speed, ya * _speed);
 			_moving = true;
 			stand = stand + 0.2;
-		}else{
+		} else {
 			_steps = 0;
 			stand--;
 			_moving = false;
 		}
 	}
+
 }
--- src/uet/oop/bomberman/entities/tile/Portal.java
+++ src/uet/oop/bomberman/entities/tile/Portal.java
@@ -4,8 +4,11 @@ import uet.oop.bomberman.Board;
 import uet.oop.bomberman.Game;
 import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.entities.bomb.Bomb;
+import uet.oop.bomberman.entities.bomb.Flame;
 import uet.oop.bomberman.entities.character.Bomber;
+import uet.oop.bomberman.entities.character.enemy.Oneal;
 import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.level.Coordinates;
 import uet.oop.bomberman.sounds.SoundFile;
 
 public class Portal extends Tile {
@@ -20,22 +23,20 @@ public class Portal extends Tile {
 	public boolean collide(Entity e) {
 		// TODO: xử lý khi Bomber đi vào
 		if(e instanceof Bomber){
-//			if(e.getXTile() == _x && e.getYTile() == _y) {
-//				if (_board.detectNoEnemies()) {
-//					if(_board.getLevel().getLevel() == Game.LASTLEVEL){
-//						//TODO: Game Finished. Phao hoa tum lum te le.
-//
-//					}
-//					else {
-//						SoundFile.next_level.play();
-//						_board.nextLevel();
-//					}
-//				}
-//			}
-			SoundFile.next_level.play();
-			_board.nextLevel();
-//			_board.getGame().player.run();
+			if(e.getXTile() == _x && e.getYTile() == _y) {
+				if (_board.detectNoEnemies()) {
+					SoundFile.next_level.play();
+					_board.nextLevel();
+				}
+			}
+//			SoundFile.next_level.play();
+//			_board.nextLevel();
 			return true;
+		}
+		if(e instanceof Flame){
+			for(int i =0;i<2;i++) {
+				_board.addCharacter(new Oneal(Coordinates.tileToPixel(_x), Coordinates.tileToPixel(_y) + Game.TILES_SIZE, _board));
+			}
 		}
 		return true;
 	}
